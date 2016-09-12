27fef9f506ae2dbd2c1fa132322fa2efd32b8821
Joshua Kilazoglou <joshua.kilazoglou@gmail.com>
2016-09-12 14:38:07 +1000

    home, list and results page included, search form from home gets passed to results via POST

--- movieapp/views.py
+++ movieapp/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 from .forms import SearchForm
 from django.http import HttpResponseRedirect, HttpResponseNotFound
+import json
+import requests
 
-
-# Create your views here.
+# Home page
 def home(request):
 
     if request.method == 'POST':
+        # unreachable code??
         form = SearchForm(request.POST)
         if form.is_valid():
             return HttpResponseRedirect('/results')
@@ -18,15 +20,31 @@ def home(request):
     return render(request, '../templates/home.html', {"bodyContent" : content})
 
 
+# Personal lists (if this is still it's own page I'm not sure)
 def lists(request):
     content = 'Some text 2'
     return render(request, '../templates/lists.html', {"bodyContent" : "List stuff"})
 
 
+# Search results
 def results(request):
     if request.method == 'POST':
-        form = SearchForm(request.POST)
+        # form = SearchForm(request.POST) -- not sure how to get this method working or if it's even necessary
+        movie_query = request.POST.get('movie-search')  # gets query from POST data
 
-    return render(request, '../templates/results.html', {"query": request.POST.get('movie-search') })
+        url = 'http://www.omdbapi.com/?s=' + movie_query
+        response = requests.get(url)
+        content = json.loads(response.text)
+        raw_items = content["Search"]
+
+        items = []
+        for item in raw_items:
+            items.append(item["Title"] + ' ' + item["Year"])
+
+
+        # title = content["Title"]
+        # year = content["Year"]
+
+    return render(request, '../templates/results.html', {"query": items})
 
 
