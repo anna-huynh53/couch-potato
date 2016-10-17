fab3de043072712eba529436665b66578e0a027b
Kayla v <Kayla@Kayla.local>
2016-10-17 20:28:43 +1100

    fix database error

--- movieapp/views.py
+++ movieapp/views.py
@@ -6,76 +6,172 @@ import requests
 from .models import Profile, Genre, Movie
 from django.contrib.auth.models import User
 from django.contrib.auth import login, logout, authenticate
+from django.db import IntegrityError
 import random
 
 # Home page
 def home(request):
 
-    if request.method == 'POST':
-        if 'logout' in request.POST:
-            request.session['loggedIn'] = False
-            logout(request)
-            return render(request, '../templates/home.html', {"loggedIn": False})
-        elif 'email' and 'username' in request.POST:
-            if User.objects.filter(email=request.POST['email']).exists():
-                print ("Email already registered");
-                return render(request, '../templates/home.html', {"addStatus": "Email already registered", "loggedIn": False})
-            else:
-                user = User.objects.create_user(username=request.POST['username'], email=request.POST['email'], password=request.POST['password'])
-                user.save() # profile supposed to be automatically created
-                login(request, user)    # login
-                print ("Created account")   # debug print
-                profile = user.profile
-                request.session['username'] = user.username
-                request.session['loggedIn'] = True
-                return render(request, '../templates/home.html', {"addStatus": "Account successfully created", "loggedIn": True})
+	if request.method == 'POST':
+		if 'logout' in request.POST:
+			request.session['loggedIn'] = False
+			logout(request)
+			return render(request, '../templates/home.html', {"loggedIn": False})
+		elif 'email' and 'username' in request.POST:
+			try:
+				user = User.objects.create_user(username=request.POST['username'], email=request.POST['email'], password=request.POST['password'])
+				user.save()
+				login(request, user)    # login
+				print ("Created account")   # debug print
+				profile = user.profile
+				request.session['username'] = user.username
+				request.session['loggedIn'] = True
+				return render(request, '../templates/home.html', {"addStatus": "Account successfully created", "loggedIn": True})
+			except IntegrityError as e:
+				if 'UNIQUE contraint' in e.message:
+					print ("Email/username already in use")
+					return render(request, '../templates/home.html', {"loggedIn": False})
 
-        elif 'username_LI' and 'password_LI' in request.POST:
-            user = authenticate(username=request.POST['username_LI'], password=request.POST['password_LI'])
-            
-            if user is None:
-                print ("Incorrect password");
-                return render(request, '../templates/home.html', {"addStatus": "Incorrect password", "loggedIn": False})
-            else:
-                login(request, user)
-                request.session['username'] = user.username
-                request.session['email'] = user.email
-                request.session['loggedIn'] = True
-                return render(request, '../templates/home.html', {"loggedIn": True})
+		elif 'username_LI' and 'password_LI' in request.POST:
+			user = authenticate(username=request.POST['username_LI'], password=request.POST['password_LI'])
+			
+			if user is None:
+				print ("Incorrect password");
+				return render(request, '../templates/home.html', {"addStatus": "Incorrect password", "loggedIn": False})
+			else:
+				login(request, user)
+				request.session['username'] = user.username
+				request.session['email'] = user.email
+				request.session['loggedIn'] = True
+				return render(request, '../templates/home.html', {"loggedIn": True})
 
-        elif 'user' in request.POST:
-            try:
-                userAdded = User.objects.get(email=request.POST['user'])
-                userAdded.save()
-                try:
-                    user = User.objects.get(email=request.session['email'])
-                    user.save()
-                    user.profile.friends.add(userAdded)
-                    user.save()
-                    return render(request, '../templates/home.html', {"addStatus": "User Added!", "loggedIn": request.session['loggedIn']})
-                except:
-                    print("Some other error")
-                    return render(request, '../templates/home.html', {"addStatus": "Error", "loggedIn": request.session['loggedIn']})
-            except:
-                print("actual user does not exist")
-                return render(request, '../templates/home.html', {"addStatus": 'User Doesnt Exist!', "loggedIn": request.session['loggedIn']})
-        else:
-            form = SearchForm(request.POST)
-            if form.is_valid():
-                return HttpResponseRedirect('/results')
-    else:   # GET method
-        if request.session.get('loggedIn'):
-            content = request.session['email']
-            user = User.objects.get(email=request.session.get('email'))
-            user.save()
-            watched = user.profile.watchedList.all()
-            toWatch = user.profile.toWatchList.all()
-            return render(request, '../templates/home.html', {"addStatus": "Email", "watched": watched, "toWatch": toWatch, "loggedIn": request.session['loggedIn']})
-        else:
-            content = 'Search'
-            return render(request, '../templates/home.html', {"addStatus" : "Email", "loggedIn": False})  
+		elif 'user' in request.POST:
+			try:
+				userAdded = User.objects.get(email=request.POST['user'])
+				userAdded.save()
+				try:
+					user = User.objects.get(email=request.session['email'])
+					user.save()
+					user.profile.friends.add(userAdded)
+					user.save()
+					return render(request, '../templates/home.html', {"addStatus": "User Added!", "loggedIn": request.session['loggedIn']})
+				except:
+					print("Some other error")
+					return render(request, '../templates/home.html', {"addStatus": "Error", "loggedIn": request.session['loggedIn']})
+			except:
+				print("actual user does not exist")
+				return render(request, '../templates/home.html', {"addStatus": 'User Doesnt Exist!', "loggedIn": request.session['loggedIn']})
+		
+		else:
+			form = SearchForm(request.POST)
+			if form.is_valid():
+				return HttpResponseRedirect('/results')
+	
+	else:   # GET method
+		if request.session.get('loggedIn'):
+			content = request.session['email']
+			user = User.objects.get(email=request.session.get('email'))
+			user.save()
+			watched = user.profile.watchedList.all()
+			toWatch = user.profile.toWatchList.all()
+			return render(request, '../templates/home.html', {"addStatus": "Email", "watched": watched, "toWatch": toWatch, "loggedIn": request.session['loggedIn']})
+		else:
+			content = 'Search'
+			return render(request, '../templates/home.html', {"addStatus" : "Email", "loggedIn": False})  
 
-    return render(request, '../templates/home.html', {"loggedIn": False})
+	return render(request, '../templates/home.html', {"loggedIn": False})
+
+
+# Personal lists (if this is still it's own page I'm not sure)
+def lists(request):
+	if request.session.get('loggedIn'):
+
+		currentUser = User.objects.get(email=request.session.get('email'))
+		return render(request, '../templates/lists.html', {"currentUserRecord": currentUser, "bodyContent": "My List"})
+
+		return render(request, '../templates/lists.html', {"bodyContent": "Error: Not logged in"})
+
+
+# Search results
+def results(request):
+	synced = False
+	# If this is a search
+	if request.method == 'POST':
+
+		if 'movie' in request.POST:
+			# if the user has clicked "add to my watched list"
+			movie = request.POST.get("movie")
+			# newMovie = Movie(title=movie.title, year=movie.year, imdbID=movie.imdbID)
+			# newMovie.save()
+			#
+			# user = User.objects.get(email=request.session.get('email'))
+			# user.watchedList.add(movie)
+			# user.save()
+		else:
+			movie_query = request.POST.get('search')  # gets query from POST data
+
+		# Search movie by Genre
+		if False: # With current way drop down menu is working, can find a way to find out which item is selected
+
+			# Sync genres if haven't yet
+			test = Genre.objects.all() # check to see if db synced with Genres
+			if not test:
+				sync_genres()
+
+			# Deal with search now
+			movie_query =str(movie_query).lower().capitalize().rstrip()
+
+			try:
+				genre = Genre.objects.get(name=movie_query)
+				url = "https://api.themoviedb.org/3/discover/movie?api_key=cc4b67c52acb514bdf4931f" \
+				"7cedfd12b&language=en-US&with_genres=" + genre.tmdbID
+			except:
+				url = "https://api.themoviedb.org/3/discover/movie?api_key=cc4b67c52acb514bdf4931" \
+				"f7cedfd12b&language=en-US&with_genres=0"
+
+				payload = "{}"
+				headers = {'content-type': 'application/json'}
+
+				response = requests.request("GET", url, data=payload, headers=headers)
+				content = json.loads(response.text)
+				raw_items = content["results"]
+
+				class Movie:
+					def __init__(self, title, year, ID, poster):
+						self.title = title
+						self.year = year
+						self.tmdbID = ID
+						self.poster = poster
+
+						movies = []
+
+						for item in raw_items:
+							movies.append(Movie(item['title'], item['release_date'], item['id'], item['poster_path']))
+
+							return render(request, '../templates/movieResults_genre.html', {"query": movies, "searchTyped": movie_query})
+
+		#search by movie title
+	else:
+		url = 'http://www.omdbapi.com/?s=' + movie_query
+		response = requests.get(url)
+		content = json.loads(response.text)
+		raw_items = content["Search"]
+
+		class Movie:
+			def __init__(self, title, year, ID, poster):
+				self.title = title
+				self.year = year
+				self.imdbID = ID
+				self.poster = poster
+
+				movies = []
+
+			# TODO make item some kind of object which includes imdbID so when clicked, it can link to more detailed info
+			for item in raw_items:
+				movies.append(Movie(item["Title"], item["Year"], item["imdbID"], item["Poster"]))
+
+		# TODO rename query
+		return render(request, '../templates/movieResults.html', {"query": movies, "searchTyped": movie_query})
 
 
 # Personal lists (if this is still it's own page I'm not sure)
@@ -363,28 +459,23 @@ def randomMovies(request):
         # and uses that number as an id
         '''movies = []
           i = 0
-
           while i < 4:
               rand_n = random.randrange(1, 4000000)
               url = "http://www.omdbapi.com/?i=tt" + str(rand_n) + "&plot=short&r=json"
               response = requests.get(url)
               content = json.loads(response.text)
-
               class Movie:
                   def __init__(self, title, year, ID, poster):
                       self.title = title
                       self.year = year
                       self.imdbID = ID
                       self.poster = poster
-
               # TODO make item some kind of object which includes imdbID so when clicked, it can link to more detailed info
               try:
                   movies.append(Movie(content["Title"], content["Year"], content["imdbID"], content["Poster"]))
               except:
                   continue
-
               i += 1
-
           '''
 
     return render(request, '../templates/randomMovies.html', {"query": movies})
